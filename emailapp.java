5e0f4553528b7321ea8581651cc15da457fe1055
Nithi <136249737+N27coder@users.noreply.github.com>
2024-02-07 23:36:18 +0530

    Add files via upload

---
+++ emailapp.java
@@ -0,0 +1,11 @@
+package Emailapp;
+
+public class emailapp {
+	
+	public static void main(String[] args) {
+	Email em1 = new Email("Nithya","Anand");
+	System.out.println(em1.showInfo());
+	}
+	
+
+}
